5f46fa2ac513d69774d1a813672aeecdcf835a40
abhishekkhemka <abhishekkhemka@gmail.com>
2013-07-21 23:39:35 +0000

    updating grader.js

--- grader.js
+++ grader.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
-
 /*
-Automatically grade files for presence of specified HTML tags/attributes.
-Uses commanded.js and cheerio. Teaches command line application development
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
 References:
 
-+ cheerio
-  - https://github.com/MatthewMueller/cheerio
-  - https://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-  - https://maxogden.com/scarping-with-node.html
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
 
-+ commander.js
-  -https://github.com/visionmedia/commander.js
-  -https://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
-+ JSON
-  -http://en.wikipedia.org/wiki/JSON
-  -https://developer.mozilla.org/en-US/docs/JSON
-  -https://developer.mozilla.org/en-US/docs/JSON#JSON_in_FireFox_2
-
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
@@ -30,12 +28,12 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
- var instr = infile.toString();
- if(!fs.existsSync(instr)) {
-     console.log("%s does not exist. Exiting.", instr);
-     process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
-   }
- return instr;
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -44,34 +42,33 @@ var cheerioHtmlFile = function(htmlfile) {
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
-}
+};
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-
-  $ = cheerioHtmlFile(htmlfile);
-  var checks = loadChecks(checksfile).sort();
-  var out = {};
-  for(var ii in checks) {
-      var present = $(checks[ii]).lenght > 0;
-      out[checks[ii]] = present;
-      }
-     return out;
-}
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
 
 var clone = function(fn) {
-    //Workaround for commander.js issue
-    //http://stackoverflow.com/a/6772648
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if (require.main == module) {
-   program
-       .option('-c, --checks <check file>','Path to checks.json', clone(assertFileExists),CHECKSFILE_DEFAULT)
-       .option('-f,--file <html_file>', 'Path to index.html',clone(assertFileExists), HTMLFILE_DEFAULT)
+if(require.main == module) {
+    program
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-       var checkJson = checkHtmlFile(program.file, program.checks);
-       var outJson = JSON.stringify(checkJson, null, 4);
-       console.log(outJson);
-     } else {
-         exports.checkHtmlFile = checkHtmlFile;
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
 }
